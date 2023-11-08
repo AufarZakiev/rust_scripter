dd2359c46c39ec22aa20c05a076df66a602d8083
Aufar Zakiev <zaufar@gmail.com>
2023-11-08 22:47:28 +0300

    Added moving input entries

--- function_widget/src/lib.rs
+++ function_widget/src/lib.rs
@@ -25,6 +25,14 @@ impl FunctionConfig {
     }
 }
 
+pub struct FunctionInputConfig {
+    input_type: ParamTypes
+}
+
+pub struct FunctionInput {
+    config: FunctionInputConfig
+}
+
 #[must_use = "You should put this widget in an ui with `ui.add(widget);`"]
 pub struct FunctionWidget<'a> {
     pub config: &'a mut FunctionConfig
@@ -40,7 +48,7 @@ impl<'a> FunctionWidget<'a> {
 
 impl Widget for &mut FunctionWidget<'_> {
     fn ui(self, ui: &mut egui::Ui) -> egui::Response {
-        let response = ui.allocate_rect(
+        let outer_response = ui.allocate_rect(
             Rect { min: self.config.position, max: Pos2 { 
                 x: self.config.position.x + self.config.sizes.x, 
                 y: self.config.position.y + self.config.sizes.y 
@@ -48,26 +56,40 @@ impl Widget for &mut FunctionWidget<'_> {
             Sense::drag()
         );
 
-        if ui.is_rect_visible(response.rect) {
+        if ui.is_rect_visible(outer_response.rect) {
             let stroke = ui.visuals().widgets.hovered.bg_stroke;
             let painter = ui.painter();
 
-            if response.hovered() {
+            if outer_response.hovered() {
                 ui.output_mut(|o| { o.cursor_icon = CursorIcon::Grab })
             }
 
-            if response.dragged() || response.drag_released() {
+            if outer_response.dragged() || outer_response.drag_released() {
                 ui.ctx().set_cursor_icon(CursorIcon::Grabbing);
 
                 if let Some(pointer_pos) = ui.ctx().pointer_interact_pos() {
-                    let delta = pointer_pos - response.rect.center();
-                    let moved_rect = response.rect.translate(delta);
+                    let delta = pointer_pos - outer_response.rect.center();
+                    let moved_rect = outer_response.rect.translate(delta);
 
                     painter.rect(
                         moved_rect, 
                         Rounding::same(5.0), 
                         Color32::from_rgb(195, 255, 104), 
                         stroke
+                    );
+
+                    painter.rect( 
+                        Rect { 
+                            min: Pos2 {
+                                x: moved_rect.min.x, 
+                                y: moved_rect.min.y + 5.0, 
+                            },
+                            max: Pos2 {
+                                x: moved_rect.min.x + 10.0, 
+                                y: moved_rect.min.y + 5.0 + 15.0, 
+                            }
+                        }, 
+                        Rounding::ZERO, Color32::from_rgb(128, 0, 0), stroke
                     );
 
                     let moved_response = ui.allocate_rect(moved_rect, Sense::drag());
@@ -79,13 +101,27 @@ impl Widget for &mut FunctionWidget<'_> {
             }
         
             painter.rect(
-                response.rect, 
+                outer_response.rect, 
                 Rounding::same(5.0), 
                 Color32::from_rgb(195, 255, 104), 
                 stroke
-            )
+            );
+
+            painter.rect( 
+                Rect { 
+                    min: Pos2 {
+                        x: outer_response.rect.min.x, 
+                        y: outer_response.rect.min.y + 5.0, 
+                    },
+                    max: Pos2 {
+                        x: outer_response.rect.min.x + 10.0, 
+                        y: outer_response.rect.min.y + 5.0 + 15.0, 
+                    }
+                }, 
+                Rounding::ZERO, Color32::from_rgb(128, 0, 0), stroke
+            );
         }
 
-        response
+        outer_response
     }
 }
