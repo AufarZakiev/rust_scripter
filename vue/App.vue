e5a4f9562319cd9e004ddd838edd8de08c495f02
Aufar Zakiev <zaufar@gmail.com>
2024-05-11 21:48:16 +0300

    Fixed Rust module load order

--- vue/App.vue
+++ vue/App.vue
@@ -1,5 +1,9 @@
 <script setup lang="ts">
 import Editor from './components/Editor.vue'
+
+import init from '/dist/rust_scripter.js';
+init('/dist/rust_scripter_bg.wasm');
+    
 </script>
 
 <template>
