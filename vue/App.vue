06253a955d5dd5cee1b56a989d1f71dce23927b3
Aufar Zakiev <zaufar@gmail.com>
2024-05-11 19:51:47 +0300

    Moved project to Vue + Rust

---
+++ vue/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import Editor from './components/Editor.vue'
+</script>
+
+<template>
+  <Editor/>
+</template>
+
+<style scoped>
+
+</style>
---
+++ vue/components/Editor.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <canvas id="the_canvas_id"></canvas>
+</template>
+
+<style scoped>
+
+#the_canvas_id {
+  width: 100% !important;
+  height: 100% !important;
+}
+
+</style>
