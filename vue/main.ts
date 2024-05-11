c3ce894480649dbdb78c6e0efa5162491d2b0dac
Aufar Zakiev <zaufar@gmail.com>
2024-05-11 22:52:03 +0300

    Added Quasar to project

--- vue/main.ts
+++ vue/main.ts
@@ -1,5 +1,11 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import { Quasar } from 'quasar'
 
-createApp(App).mount('#app')
+// Import icon libraries
+import '@quasar/extras/material-icons/material-icons.css'
+
+createApp(App).use(Quasar, {
+    plugins: {}, // import Quasar plugins and add here
+}).mount('#app')
